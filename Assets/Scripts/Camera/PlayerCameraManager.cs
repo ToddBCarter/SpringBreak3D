05945d2850a2a477f3f22ee3c99d3f09c76706e2
tCarter <tcart00990@gmail.com>
2026-03-18 11:20:56 -0700

    Cleaning up.
    Clean up after Cinemachine implemented.

--- Assets/Scripts/Camera/PlayerCameraManager.cs
+++ Assets/Scripts/Camera/PlayerCameraManager.cs
@@ -4,22 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerCameraManager : MonoBehaviour
-{	
-/*
-    private const float YMin = -85.0f;
-    private const float YMax = 85.0f;
-
-    public Transform lookAt;
-
-    public Transform Player;
-
-    public float distance = 10.0f;
-    private float currentX = 0.0f;
-    private float currentY = 0.0f;
-    public float sensivity = 1.0f;
-
-*/
-
+{
 	//Returns the forward direction of the camera for the player's movement
     public Vector3 CameraForward
     {
@@ -31,22 +16,4 @@ public class PlayerCameraManager : MonoBehaviour
         }
     }
 
-/*
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        currentX += Mouse.current.delta.x.ReadValue() * sensivity;
-        currentY += -(Mouse.current.delta.y.ReadValue() * sensivity);
-
-        currentY = Mathf.Clamp(currentY, YMin, YMax);
-
-        Vector3 Direction = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        transform.position = lookAt.position + rotation * Direction;
-
-        transform.LookAt(lookAt.position);
-
-    }
-	
-	*/
 }
